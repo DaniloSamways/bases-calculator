53de934e336e376661d4fbf61a26d04e62e8dfa3
Danilo Samways <hadeflowing@gmail.com>
2023-02-17 00:53:18 -0300

    Added social links

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
-import './App.css'
 import { Selection } from './components/Selection/selection';
 import { decimalToBase } from './utils/decimalToBase';
+import GithubIcon from './assets/github.svg';
+import InstagramIcon from './assets/instagram.svg';
+
+import './styles/author.css'
+import './App.css'
+import { GitHubLogoIcon, InstagramLogoIcon } from '@radix-ui/react-icons';
 
 type convertionProps = {
   result: string | number;
@@ -30,41 +35,53 @@ function App() {
   }
 
   return (
-    <main>
-      <h2>Selecione as bases para converter</h2>
+    <>
 
-      <form onSubmit={(event) => event.preventDefault()}>
-        <div className="selections">
-          <Selection
-            base={initialBase}
-            setBase={setInitialBase}
-            otherBase={finalBase}
-          />
-          <span className="divider">para</span>
-          <Selection
-            base={finalBase}
-            setBase={setFinalBase}
-            otherBase={initialBase}
-          />
-        </div>
+      <main>
+        <h2>Selecione as bases para converter</h2>
 
-        <div className="insertion">
-          <input
-            value={value}
-            onChange={e => setValue(e.target.value)}
-            type="text"
-            placeholder="Valor a converter"
-          />
-          <button onClick={handleConvert}>Converter</button>
-        </div>
-      </form>
+        <form onSubmit={(event) => event.preventDefault()}>
+          <div className="selections">
+            <Selection
+              base={initialBase}
+              setBase={setInitialBase}
+              otherBase={finalBase}
+            />
+            <span className="divider">para</span>
+            <Selection
+              base={finalBase}
+              setBase={setFinalBase}
+              otherBase={initialBase}
+            />
+          </div>
 
-      <pre>
-        <h3>RESOLUÇÃO</h3>
-        <p>{convertion.resolution}</p>
-        <div><span>RESULTADO:</span> {convertion.result}</div>
-      </pre>
-    </main>
+          <div className="insertion">
+            <input
+              value={value}
+              onChange={e => setValue(e.target.value)}
+              type="text"
+              placeholder="Valor a converter"
+            />
+            <button onClick={handleConvert}>Converter</button>
+          </div>
+        </form>
+
+        <pre>
+          <h3>RESOLUÇÃO</h3>
+          <p>{convertion.resolution}</p>
+          <div><span>RESULTADO:</span> {convertion.result}</div>
+        </pre>
+      </main>
+
+      <footer>
+        <a href="https://github.com/DaniloSamways" target="_blank">
+          <GitHubLogoIcon color='white' height={35} width={35} />
+        </a>
+        <a href="https://www.instagram.com/danilo.samw/" target="_blank">
+          <InstagramLogoIcon color='white' height={35} width={35} />
+        </a>
+      </footer>
+    </>
   )
 }
 
